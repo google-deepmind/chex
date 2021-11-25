07058d7f5950fa4b0abaf34078488d6d27a89f1b
Iurii Kemaev <iukemaev@google.com>
2021-11-25 03:54:54 -0800

    WIP
    PiperOrigin-RevId: 412250509

--- chex/_src/asserts_internal.py
+++ chex/_src/asserts_internal.py
@@ -24,29 +24,112 @@ Instead, consider opening an issue on GitHub and describing your use case.
 
 import collections
 import collections.abc
+import dataclasses
 import functools
 import re
-from typing import Any, Sequence, Union, Callable, Optional, Set, Tuple, Type
+import threading
+import traceback
+from typing import Any, Callable, List, Optional, Sequence, Set, Tuple, Type, Union
 
 from absl import logging
 from chex._src import pytypes
 import jax
+from jax.experimental import host_callback as hcb
 import jax.numpy as jnp
 import numpy as np
+
+_lock = threading.Lock()
+_internal_error_message = (
+    "Internal correctness check failed. Please file an issue on Chex bug "
+    "tracker and describe how to reproduce this failure.")
 
 # Custom pytypes.
 TLeaf = Any
 TLeavesEqCmpFn = Callable[[TLeaf, TLeaf], bool]
 TLeavesEqCmpErrorFn = Callable[[TLeaf, TLeaf], str]
 
+# TODO(iukemaev): define a typing protocol for TChexAssertion.
+# Chex assertion signature:
+# (*args,
+#  custom_message: Optional[str] = None,
+#  custom_message_format_vars: Sequence[Any] = (),
+#  include_default_message: bool = True,
+#  exception_type: Type[Exception] = AssertionError,
+#  **kwargs)
+TChexAssertion = Callable[..., None]
+TAssertFn = Callable[..., None]
+TJittableAssertFn = Callable[..., bool]
+
 # Matchers.
 TDimMatcher = Optional[Union[int, Set[int], type(Ellipsis)]]
 TShapeMatcher = Sequence[TDimMatcher]
 
 # Chex namespace variables.
-ERR_PREFIX = "[Chex] "
-TRACE_COUNTER = collections.Counter()
-DISABLE_ASSERTIONS = False
+ERR_PREFIX: str = "[Chex] "
+TRACE_COUNTER: collections.Counter = collections.Counter()
+DISABLE_ASSERTIONS: bool = False
+
+# Chex assertions internals.
+IS_CHEX_CONTEXT: bool = False
+ERRORS_REGISTRY: List["ValueAssertionError"] = []
+VALUE_ASSERTION_METADATA_REGISTRY: List["ValueAssertionMetadata"] = []
+VALUE_ASSERTION_DEPS_CHAIN: Optional["DependencyChain"] = None
+
+
+@dataclasses.dataclass(frozen=True)
+class ValueAssertionMetadata:
+  """A dataclass to store a value assertion' metadata."""
+  assertion_id: int
+  stack_trace: Sequence[str]
+  custom_message: Optional[str]
+  include_default_message: bool
+  exception_type: Type[Exception]
+
+  def format_stack_trace(self) -> str:
+    return "\n".join(self.stack_trace)
+
+
+@dataclasses.dataclass(frozen=True)
+class ValueAssertionError:
+  """A dataclass to store the information about a failed value assertion."""
+  assertion_id: int
+  message: str
+  device: pytypes.Device
+
+  def format_error(self) -> str:
+    metadata = VALUE_ASSERTION_METADATA_REGISTRY[self.assertion_id]
+    stack_trace = metadata.format_stack_trace()
+    return f"{stack_trace}{self.message} [on device '{self.device}']"
+
+
+class DependencyChain:
+  """A class that maintains a chain of (HLO) control dependencies.
+
+  Used to inject additional dependencies into the outputs of the jitted
+  functions to work around DCE optimization for value assertions.
+  """
+
+  def __init__(self) -> None:
+    self._head_elem = None
+    self._is_attached = False
+
+  def append(self, tree: pytypes.ArrayTree) -> None:
+    """Appends the tree leaves to the chain."""
+    if self._is_attached:
+      raise RuntimeError("Dependency chain has already been attached.")
+    for x in jax.tree_leaves(tree):
+      if self._head_elem is None:
+        self._head_elem = x
+      else:
+        self._head_elem = hcb.id_tap_dep_p.bind(self._head_elem, x)
+
+  def attach_to(self, tree: pytypes.ArrayTree) -> pytypes.ArrayTree:
+    self._is_attached = True
+    if self._head_elem is None:
+      return tree
+    else:
+      return jax.tree_map(lambda x: hcb.id_tap_dep_p.bind(x, self._head_elem),
+                          tree)
 
 
 def jnp_to_np_array(arr: pytypes.Array) -> pytypes.Array:
@@ -86,31 +169,121 @@ def get_err_regex(message: str) -> str:
   return f"{re.escape(ERR_PREFIX)}[\\s\\S]*{message}"
 
 
-def chex_assertion(assert_fn: Callable[..., None]) -> Callable[..., None]:
-  """Wraps Chex assert functions to control their common behaviour.
+def _extract_stack_trace():
+  return traceback.format_stack()[-6:-3]  # last 3 external frames
 
-  Extends the assertion to support the following optional auxiliary kwargs:
-    custom_message: A string to include into the emitted exception messages.
-    include_default_message: Whether to include the default Chex message into
-      the emitted exception messages.
-    exception_type: An exception type to use. `AssertionError` by default.
+
+def _extract_errors_from_registry() -> Exception:
+  with _lock:
+    errors = [err.format_error() for err in ERRORS_REGISTRY]
+    ERRORS_REGISTRY.clear()
+    return AssertionError("The following errors were detected:\n" +
+                          ("\n" + "-" * 40 + "\n").join(errors))
+
+
+def make_value_assertion(
+    host_assertion: TChexAssertion,
+    jittable_assert_fn: TJittableAssertFn) -> TChexAssertion:
+  """Constructs a value assertion.
+
+  Value assertions can be used in compiled (i.e. jitted) functions.
 
   Args:
-    assert_fn: An assertion function.
+    host_assertion: an assertion to use on host.
+    jittable_assert_fn: an assertion to use on device.
 
   Returns:
-   A wrapped assertion function with the auxiliary kwargs.
+    A chex value assertion.
   """
 
-  @functools.wraps(assert_fn)
-  def _chex_assertion(*args,
-                      custom_message: Optional[str] = None,
-                      include_default_message: bool = True,
-                      exception_type: Type[Exception] = AssertionError,
-                      **kwargs) -> None:
-    if DISABLE_ASSERTIONS:
-      return
+  def _host_failure_handler(arg, transforms, *, device=None):
+    del transforms
 
+    (assertion_id, custom_message_format_vars, args, kwargs) = arg
+    if not assertion_id < len(VALUE_ASSERTION_METADATA_REGISTRY):
+      # All assertions must be registered after tracing.
+      raise RuntimeError(_internal_error_message)
+    assertion_meta = VALUE_ASSERTION_METADATA_REGISTRY[assertion_id]
+
+    error_msg = None
+    try:
+      host_assertion(
+          *args,
+          custom_message=assertion_meta.custom_message,
+          custom_message_format_vars=custom_message_format_vars,
+          include_default_message=assertion_meta.include_default_message,
+          exception_type=assertion_meta.exception_type,
+          **kwargs)
+    except Exception as e:  # pylint: disable=broad-except
+      error_msg = str(e)
+
+    if error_msg is None:
+      raise RuntimeError(f"Host could not reproduce the failure! Stacktrace:\n"
+                         f"{assertion_meta.format_stacktrace()}")
+
+    with _lock:
+      ERRORS_REGISTRY.append(
+          ValueAssertionError(
+              assertion_id=assertion_id, message=error_msg, device=device))
+
+  def _jittable_assertion(*args,
+                          custom_message: Optional[str] = None,
+                          custom_message_format_vars: Sequence[Any] = (),
+                          include_default_message: bool = True,
+                          exception_type: Type[Exception] = AssertionError,
+                          **kwargs) -> None:
+    # Save assertion's metadata during tracing.
+    assertion_id = len(VALUE_ASSERTION_METADATA_REGISTRY)
+    VALUE_ASSERTION_METADATA_REGISTRY.append(
+        ValueAssertionMetadata(
+            assertion_id=assertion_id,
+            stack_trace=_extract_stack_trace(),
+            custom_message=custom_message,
+            include_default_message=include_default_message,
+            exception_type=exception_type))
+
+    # Tie new dependencies into tracers from the inputs.
+    tie_in = get_tracers((args, kwargs))
+    if not tie_in:
+      # Value asserts should only be triggered for inputs with tracers.
+      raise RuntimeError(_internal_error_message)
+
+    def _passed_branch(tie_in):
+      return tie_in
+
+    def _failed_branch(tie_in):
+      return hcb.id_tap(
+          _host_failure_handler,
+          arg=(assertion_id, custom_message_format_vars, args, kwargs),
+          result=tie_in,
+          tap_with_device=True)
+
+    is_passed = jittable_assert_fn(*args, **kwargs)
+    dependency = jax.lax.cond(is_passed, _passed_branch, _failed_branch, tie_in)
+    VALUE_ASSERTION_DEPS_CHAIN.append(dependency)  # only during tracing
+
+  return _jittable_assertion
+
+
+def make_static_assertion(assert_fn: TAssertFn) -> TChexAssertion:
+  """Constructs a static assertion from an assert function.
+
+  This wrapper should be used for assertions that do not check values or are
+  not used in jitted code.
+
+  Args:
+    assert_fn: a function implementing the check.
+
+  Returns:
+    A chex assertion.
+  """
+
+  def _static_assert(*args,
+                     custom_message: Optional[str] = None,
+                     custom_message_format_vars: Sequence[Any] = (),
+                     include_default_message: bool = True,
+                     exception_type: Type[Exception] = AssertionError,
+                     **kwargs) -> None:
     # Format error's stack trace to remove Chex' internal frames.
     assertion_exc = None
     value_exc = None
@@ -139,11 +312,128 @@ def chex_assertion(assert_fn: Callable[..., None]) -> Callable[..., None]:
 
         # Whether to include a custom error message.
         if custom_message:
+          if custom_message_format_vars:
+            custom_message = custom_message.format(*custom_message_format_vars)
           error_msg = f"{error_msg} [{custom_message}]"
 
         raise exception_type(error_msg)
 
+  return _static_assert
+
+
+def chex_assertion(
+    assert_fn: TAssertFn,
+    jittable_assert_fn: Optional[TJittableAssertFn] = None) -> TChexAssertion:
+  """Wraps Chex assert functions to control their common behaviour.
+
+  Extends the assertion to support the following optional auxiliary kwargs:
+    custom_message: A string to include into the emitted exception messages.
+    custom_message_format_vars: A list of variables to pass as arguments to
+      `custom_message.format()`.
+    include_default_message: Whether to include the default Chex message into
+      the emitted exception messages.
+    exception_type: An exception type to use. `AssertionError` by default.
+
+  Args:
+    assert_fn: a function implementing the check.
+    jittable_assert_fn: an optional jittable version of `assert_fn`. Required
+      for value assertions.
+
+  Returns:
+    A Chex assertion (with auxiliary kwargs).
+  """
+
+  host_assertion = make_static_assertion(assert_fn)
+  is_value_assertion = (jittable_assert_fn or assert_fn) is not assert_fn
+  if is_value_assertion:
+    # We do not verify that `jittable_assert_fn` is jittable.
+    value_assertion = make_value_assertion(host_assertion, jittable_assert_fn)
+
+  @functools.wraps(assert_fn)
+  def _chex_assertion(*args, **kwargs) -> None:
+    if DISABLE_ASSERTIONS:
+      return
+
+    if is_value_assertion and has_tracers((args, kwargs)):
+      if not IS_CHEX_CONTEXT:
+        raise RuntimeError(
+            "Value assertions can only be called from functions wrapped "
+            "with `@chex.with_value_assertions`. See the docs.")
+
+      assert VALUE_ASSERTION_DEPS_CHAIN is not None
+      value_assertion(*args, **kwargs)
+    else:
+      host_assertion(*args, **kwargs)
+
   return _chex_assertion
+
+
+def with_value_assertions(pure_fn, apply_transform, barrier_wait=True):
+  """Wraps a jitted function to post-process the value assertions' results.
+
+  Args:
+    pure_fn: a pure (unjitted) python function to transform.
+    apply_transform: a chain of JAX transformations to appy to `pure_fn`.
+    barrier_wait: a bool suggesting whether to wait until all host callbacks
+      complete before returning the outputs. Warning: prevents run-ahead exec.
+      strategy, see https://jax.readthedocs.io/en/latest/async_dispatch.html.
+
+  Returns:
+    A transformed function that supports value assertions.
+  """
+
+  # Check wrappers ordering.
+  if is_traceable(pure_fn):
+    raise ValueError(
+        "@with_value_assertions must not wrap JAX-transformed functions. "
+        "Change wrappers ordering.")
+
+  def _pure_fn_with_folded_deps(*args, **kwargs):
+    if not IS_CHEX_CONTEXT:
+      # This func should only be called from `_transformed_fn_with_sentinel`.
+      raise RuntimeError(_internal_error_message)
+
+    global VALUE_ASSERTION_DEPS_CHAIN
+    if VALUE_ASSERTION_DEPS_CHAIN is not None:
+      # Only this func is allowed to manipulate `VALUE_ASSERTION_DEPS_CHAIN`.
+      raise RuntimeError(_internal_error_message)
+    VALUE_ASSERTION_DEPS_CHAIN = DependencyChain()
+
+    outputs = pure_fn(*args, **kwargs)
+    outputs_with_deps = VALUE_ASSERTION_DEPS_CHAIN.attach_to(outputs)
+    VALUE_ASSERTION_DEPS_CHAIN = None
+    return outputs_with_deps
+
+  transformed_fn = apply_transform(_pure_fn_with_folded_deps)
+
+  @functools.wraps(pure_fn)
+  def _transformed_fn_with_sentinel(*args, **kwargs):
+    # Check on errors from the previous calls (e.g. when `barrier_wait==False`).
+    if ERRORS_REGISTRY:
+      raise _extract_errors_from_registry()
+
+    global IS_CHEX_CONTEXT
+    if IS_CHEX_CONTEXT:
+      # Only this function is allowed to switch `IS_CHEX_CONTEXT`.
+      raise RuntimeError(_internal_error_message)
+    try:
+      IS_CHEX_CONTEXT = True
+      output = transformed_fn(*args, **kwargs)
+    finally:
+      IS_CHEX_CONTEXT = False
+      global VALUE_ASSERTION_DEPS_CHAIN
+      VALUE_ASSERTION_DEPS_CHAIN = None
+
+    if barrier_wait:
+      hcb.barrier_wait()  # Optional. Waits until all callbacks are completed.
+
+    # Check on new errors.
+    if ERRORS_REGISTRY:
+      raise _extract_errors_from_registry()
+
+    return output
+
+  return _transformed_fn_with_sentinel
 
 
 def format_tree_path(path: Sequence[Any]) -> str:
@@ -176,7 +466,7 @@ def has_tracers(tree: pytypes.ArrayTree) -> bool:
   return any(isinstance(x, jax.core.Tracer) for x in jax.tree_leaves(tree))
 
 
-def is_traceable(fn):
+def is_traceable(fn) -> bool:
   """Checks if function is traceable.
 
   JAX traces a function when it is wrapped with @jit, @pmap, or @vmap.
--- chex/_src/asserts_internal_test.py
+++ chex/_src/asserts_internal_test.py
@@ -15,12 +15,16 @@
 """Tests for `asserts_internal.py`."""
 
 import functools
+import re
 
 from absl.testing import absltest
 from absl.testing import parameterized
 from chex._src import asserts_internal as ai
+from chex._src import pytypes
 from chex._src import variants
 import jax
+import jax.numpy as jnp
+import numpy as np
 
 
 class IsTraceableTest(variants.TestCase):
@@ -64,7 +68,8 @@ class ExceptionMessageFormatTest(variants.TestCase):
       include_custom_msg=(False, True),
       exc_type=(AssertionError, ValueError),
   )
-  def test_format(self, include_default_msg, include_custom_msg, exc_type):
+  def test_format_static_assertion(self, include_default_msg,
+                                   include_custom_msg, exc_type):
 
     exc_msg = lambda x: f'{x} is non-positive.'
 
@@ -109,6 +114,253 @@ class ExceptionMessageFormatTest(variants.TestCase):
             custom_message=custom_msg,
             include_default_message=include_default_msg,
             exception_type=exc_type)
+
+  @variants.variants(with_jit=True, with_pmap=True)
+  @parameterized.product(
+      include_default_msg=(False, True),
+      include_custom_msg=(False, True),
+      exc_type=(AssertionError, ValueError),
+  )
+  def test_format_value_assertion(self, include_default_msg, include_custom_msg,
+                                  exc_type):
+    # Define a chex assertion.
+    exc_msg = lambda x: f'{x} is non-positive.'
+
+    def assert_positive(x):
+      if x <= 0:
+        raise AssertionError(exc_msg(x))
+
+    def jittable_assert_positive(x):
+      return (x > 0).all()
+
+    chex_assert_positive = ai.chex_assertion(assert_positive,
+                                             jittable_assert_positive)
+
+    # Define a function with custom messages.
+    n_steps = 3
+    custom_msg = 'failed at iter {}' if include_custom_msg else ''
+    n_traces = 0
+
+    def pure_fn(x_pos, x_neg, start_from):
+      nonlocal n_traces
+      n_traces += 1
+
+      y = x_pos
+      # Run in a loop to generate different custom messages.
+      for i in range(n_steps):
+        step = start_from + i
+        # Raises exception #1: `expected_exc_msg(x_neg, i)`.
+        chex_assert_positive(  # pylint:disable=unexpected-keyword-arg
+            x_neg,
+            custom_message=custom_msg,
+            custom_message_format_vars=[step],  # `step` is traced
+            include_default_message=include_default_msg,
+            exception_type=exc_type)
+
+        # Passes.
+        chex_assert_positive(x_pos)
+
+        # Raises exception #2: `expected_exc_msg(3 * x_neg, custom_msg)`.
+        chex_assert_positive(  # pylint:disable=unexpected-keyword-arg
+            3 * x_neg,
+            custom_message=custom_msg,
+            custom_message_format_vars=[step],  # `step` is traced
+            include_default_message=include_default_msg,
+            exception_type=exc_type)
+        y += x_pos + x_neg + i
+      return y
+
+    fn = ai.with_value_assertions(pure_fn, self.variant)
+
+    # Check the format of raised exceptions' messages.
+    def expected_exc_msg(x, custom_msg, device):
+      msg = exc_msg(x) if include_default_msg else ''
+      msg = rf'.*{msg} \[{custom_msg}\]' if custom_msg else msg
+      return msg + f'.*{re.escape(str(device))}'
+
+    # Check that the error is reported for every participating device.
+    if self.variant is variants.ChexVariantType.WITH_PMAP:
+      devices = jax.local_devices()
+    else:
+      devices = jax.local_devices()[0:1]
+
+    # Check that 2 exceptions are raise at _every_ step (out of `n_steps`).
+    # Value assertions are merged into one AssertionError, so we call the same
+    # function multiple times but check presence of different a-n messages.
+    for i in range(n_steps):
+      start_from = 2 + i
+      step_msg = custom_msg.format(start_from + i)
+      x_pos = 3 * i
+      x_neg = -2 * (i + 1)
+
+      for device in devices:
+        # Exception #1.
+        with self.assertRaisesRegex(
+            AssertionError,  # Value assertions raise `AssertionError`s.
+            ai.get_err_regex(expected_exc_msg(x_neg, step_msg, device))):
+          fn(x_pos, x_neg, start_from)
+
+        # Exception #2.
+        with self.assertRaisesRegex(
+            AssertionError,  # Value assertions raise `AssertionError`s.
+            ai.get_err_regex(expected_exc_msg(3 * x_neg, step_msg, device))):
+          fn(x_pos, x_neg, start_from)
+
+    self.assertEqual(n_traces, 1)
+
+
+def _assert_trees_equal(tree_1: pytypes.ArrayTree,
+                        tree_2: pytypes.ArrayTree) -> None:
+
+  def _assert_fn(x1, x2):
+    np.testing.assert_array_equal(
+        ai.jnp_to_np_array(x1), ai.jnp_to_np_array(x2))
+
+  jax.tree_map(_assert_fn, tree_1, tree_2)
+
+
+class ChexAssertionsTest(variants.TestCase):
+  """Tests for Chex assertions."""
+
+  def setUp(self):
+    super().setUp()
+
+    def assert_tree_isfinite(tree):
+      # Use jnp instead of np for testing purposes.
+      if not all(jnp.isfinite(x).all() for x in jax.tree_leaves(tree)):
+        raise AssertionError('Tree contains NaNs!')
+
+    def jittable_assert_tree_isfinite(tree):
+      # must be jittable
+      return jnp.all(
+          jnp.array([jnp.isfinite(x).all() for x in jax.tree_leaves(tree)]))
+
+    self.chex_assert_isfinite_without_value = ai.chex_assertion(
+        assert_tree_isfinite)
+
+    self.chex_assert_isfinite_with_value = ai.chex_assertion(
+        assert_tree_isfinite, jittable_assert_tree_isfinite)
+
+  @variants.variants(with_jit=True, with_pmap=True)
+  def test_value_assertion(self):
+    eps = 1e-7
+
+    def _make_log_abs_fn(assert_input_fn: ai.TChexAssertion):
+
+      def _pure_log_fn(tree_1, tree_2):
+        # Call twice to make sure all deps are retained after XLA optimizations.
+        assert_input_fn(tree_1)
+        assert_input_fn(tree_2)
+
+        return jax.tree_map(lambda x1, x2: jnp.log(jnp.abs(x1 + x2) + eps),
+                            tree_1, tree_2)
+
+      return _pure_log_fn
+
+    # Construct 3 versions of `log(|x1 + x2|)` function.
+    log_abs_fn_no_assert = _make_log_abs_fn(lambda x: None)
+    log_abs_fn_value_assert = _make_log_abs_fn(
+        self.chex_assert_isfinite_with_value)
+    log_abs_fn_static_assert = _make_log_abs_fn(
+        self.chex_assert_isfinite_without_value)
+
+    # Define correct and incorrect inputs.
+    x = {'a': np.zeros((2,)), 'b': {'c': np.array([1, 2])}}
+    x_with_nan = {'a': np.zeros((2,)), 'b': {'c': np.array([1, np.nan])}}
+
+    # Test all versions return the same outputs.
+    _assert_trees_equal(
+        log_abs_fn_no_assert(x, x), log_abs_fn_static_assert(x, x))
+    _assert_trees_equal(
+        log_abs_fn_no_assert(x, x), log_abs_fn_value_assert(x, x))
+
+    # Test on-device assertion.
+    _assert_trees_equal(
+        log_abs_fn_no_assert(x, x),
+        ai.with_value_assertions(log_abs_fn_value_assert, self.variant)(x, x))
+
+    # `ConcretizationTypeError` when static assertion is used in value checks.
+    with self.assertRaisesRegex(
+        jax.errors.ConcretizationTypeError,
+        'Abstract tracer value encountered where concrete value is expected'):
+      self.variant(log_abs_fn_static_assert)(x, x)
+    with self.assertRaisesRegex(
+        jax.errors.ConcretizationTypeError,
+        'Abstract tracer value encountered where concrete value is expected'):
+      self.variant(log_abs_fn_static_assert)(x, x_with_nan)
+
+    # On-device assertion passes.
+    _assert_trees_equal(
+        log_abs_fn_no_assert(x, x),
+        ai.with_value_assertions(log_abs_fn_value_assert, self.variant)(x, x))
+
+    # Static assertion fails on incorrect inputs (without transformations).
+    with self.assertRaisesRegex(AssertionError, 'Tree contains NaNs!'):
+      log_abs_fn_static_assert(x_with_nan, x)
+
+    with self.assertRaisesRegex(AssertionError, 'Tree contains NaNs!'):
+      log_abs_fn_static_assert(x, x_with_nan)
+
+    # Value assertion fails on incorrect inputs (with transformations).
+    transformed_log_abs_fn_value_assert = ai.with_value_assertions(
+        log_abs_fn_value_assert, self.variant)
+
+    # Check that the error is reported for every participating device.
+    if self.variant is variants.ChexVariantType.WITH_PMAP:
+      devices = jax.local_devices()
+    else:
+      devices = jax.local_devices()[0:1]
+    for device in devices:
+      message_regexp = f'Tree contains NaNs!.*{re.escape(str(device))}'
+      with self.assertRaisesRegex(AssertionError, message_regexp):
+        transformed_log_abs_fn_value_assert(x, x_with_nan)
+      with self.assertRaisesRegex(AssertionError, message_regexp):
+        transformed_log_abs_fn_value_assert(x_with_nan, x)
+      with self.assertRaisesRegex(AssertionError, message_regexp):
+        transformed_log_abs_fn_value_assert(x_with_nan, x_with_nan)
+
+    # Reports incorrect usage without `with_value_assertions()`.
+    with self.assertRaisesRegex(
+        RuntimeError,
+        'can only be called from functions wrapped .*with_value_assertions'):
+      self.variant(log_abs_fn_value_assert)(x, x_with_nan)
+
+  @variants.variants(with_jit=True, without_jit=True)
+  def test_static_assertion(self):
+    # Tests that static assertions can be used without `with_value_assertions`.
+    shape = (2, 3)
+
+    # Define a simple static assertion.
+    @ai.chex_assertion
+    def chex_assert_shape(array, expected):
+      if array.shape != expected:
+        raise AssertionError('Wrong shape!')
+
+    # Define a simple function that uses the assertion.
+    def _sum_fn(tree):
+      jax.tree_map(lambda x: chex_assert_shape(x, shape), tree)
+      return sum(x.sum() for x in jax.tree_leaves(tree))
+
+    # Passes in all contexts.
+    x = {'a': np.ones(shape), 'b': {'c': np.ones(shape)}}
+    self.assertEqual(_sum_fn(x), 2 * np.prod(shape))
+    self.assertEqual(self.variant(_sum_fn)(x), 2 * np.prod(shape))
+    self.assertEqual(
+        ai.with_value_assertions(_sum_fn, self.variant)(x), 2 * np.prod(shape))
+
+    # Fails in all contexts.
+    x_wrong_shape = {'a': np.ones(shape), 'b': {'c': np.ones(shape + shape)}}
+    with self.assertRaisesRegex(AssertionError, 'Wrong shape!'):
+      _sum_fn(x_wrong_shape)
+    with self.assertRaisesRegex(AssertionError, 'Wrong shape!'):
+      self.variant(_sum_fn)(x_wrong_shape)
+    with self.assertRaisesRegex(AssertionError, 'Wrong shape!'):
+      ai.with_value_assertions(_sum_fn, self.variant)(x_wrong_shape)
+
+  def test_jitted_fn_to_transform_fail(self):
+    with self.assertRaisesRegex(ValueError,
+                                'must not wrap JAX-transformed functions'):
+      ai.with_value_assertions(jax.jit(lambda x: x.sum()), jax.pmap)
 
 
 if __name__ == '__main__':
