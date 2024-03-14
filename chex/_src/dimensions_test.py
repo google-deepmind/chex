ae6efbf7a6141d026a92047cb2baa9d2b67f8649
Kristian Holsheimer <holsheimer@google.com>
2024-03-14 04:33:09 -0700

    Add Dimensions.size to get flat size from named shape.
    PiperOrigin-RevId: 615731880

--- chex/_src/dimensions_test.py
+++ chex/_src/dimensions_test.py
@@ -91,6 +91,14 @@ class DimensionsTest(parameterized.TestCase):
     with self.assertRaisesRegex(e, m):
       dims[k]  # pylint: disable=pointless-statement
 
+  @parameterized.named_parameters([
+      ('scalar', '', (), 1),
+      ('nonscalar', 'ab', (3, 5), 15),
+  ])
+  def test_size_ok(self, names, shape, expected_size):
+    dims = dimensions.Dimensions(**dict(zip(names, shape)))
+    self.assertEqual(dims.size(names), expected_size)
+
 
 if __name__ == '__main__':
   absltest.main()
