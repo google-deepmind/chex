a09fc359538fe4ee7ba5e2b93716e63f10f8305d
Iurii Kemaev <iukemaev@google.com>
2026-06-12 07:16:31 -0700

    Release v0.1.92
    PiperOrigin-RevId: 931136482

--- chex/__init__.py
+++ chex/__init__.py
@@ -105,7 +105,7 @@ from chex._src.warnings import warn_keyword_args_only_in_future
 from chex._src.warnings import warn_only_n_pos_args_in_future
 
 
-__version__ = "0.1.91"
+__version__ = "0.1.92"
 
 __all__ = (
     "all_variants",
